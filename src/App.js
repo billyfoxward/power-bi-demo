b742bcc98f2b7e562442b1d4ef43c06bd5fc98f0
Billy Ward <billyward@richmondfellowship.org.uk>
2022-04-05 23:07:42 +0100

    Embedding Power BI report

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <iframe title="Billy Ward - Demo Report" width="100%" height="850" src="https://app.powerbi.com/reportEmbed?reportId=8e2a66a8-4b53-4f1d-b0bd-591ddf209c12&autoAuth=true&ctid=221de6f9-c50e-489b-b491-89fbe7733a3e&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly8yMjFkZTZmOWM1MGU0ODliYjQ5MTg5ZmJlNzczM2EzZS1hcGkuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" frameborder="0" allowFullScreen="true"></iframe>
       </header>
     </div>
   );
